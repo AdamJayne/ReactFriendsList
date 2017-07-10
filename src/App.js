1c52afe0d6c74ff340340e4576b09640f35aa743
Adam Jayne <adamjayne402@gmail.com>
2017-07-10 13:29:23 -0400

    Added header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      
-    );
-  }
+	render() {
+		return (
+			<div>
+				{this.props.children}
+			</div>
+		);
+	}
 }
 
 export default App;
