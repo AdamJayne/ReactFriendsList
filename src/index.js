82e1bbc8a5e9bd74317d590d56ac59c573acecc0
Adam Jayne <adamjayne402@gmail.com>
2017-07-10 14:00:23 -0400

    added friend list scaffold

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Header from './Header';
+import FriendList from './FriendList'
 import './index.css';
 
 ReactDOM.render(
 	<div className ="row">
 		<App>
 			<Header />
+			<FriendList />
 		</App>
 	</div>,
 	document.getElementById('root')
